07ef63b4eedbf6fd803efe66b3cf4f4181ac1589
NGabrielHrz <rotsen.soher@gmail.com>
2023-06-01 14:34:39 -0600

    Creacion del menu interactivo para el 'Proyecto: Uni'

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="description" content="">
+  <link rel="stylesheet" href="style.css">
+  <title>University</title>
+</head>
+<body>
+  <script src="script.js"></script>
+  <?php
+  include "menu.php";
+  ?>
+  <section class="home">
+    <div class="text">Barra lateral del tablero</div>
+  </section>
+</body>
+</html>
---
+++ menu.php
@@ -0,0 +1,78 @@
+<nav class="sidebar close">
+  <header>
+    <div class="image-text">
+      <span class="image">
+        <img src="img/logo_tec.png" alt="">
+      </span>
+      <div class="text logo-text">
+        <span class="name"></span>
+        <span class="profession">Tecnm</span>
+      </div>
+    </div>
+    <i class="bx bx-chevron-right toggle"></i>
+  </header>
+  <div class="menu-bar">
+    <div class="menu">
+      <li class="search-box">
+        <i class="bx bx-search icon"></i>
+        <input type="text" placeholder="Buscar...">
+      </li>
+      <ul class="menu-links">
+        <li class="nav-links">
+          <a href="#">
+            <i class="bx bx-home-alt icon"></i>
+            <span class="text nav-text">Panel</span>
+          </a>
+        </li>
+        <li class="nav-links">
+          <a href="#">
+            <i class="bx bx-bar-chart-alt-2 icon"></i>
+            <span class="text nav-text">Ganancia</span>
+          </a>
+        </li>
+        <li class="nav-links">
+          <a href="#">
+            <i class="bx bx-bell icon"></i>
+            <span class="text nav-text">Notificaciones</span>
+          </a>
+        </li>
+        <li class="nav-links">
+          <a href="#">
+            <i class="bx bx-pie-chart-alt icon"></i>
+            <span class="text nav-text">Analizis</span>
+          </a>
+        </li>
+        <li class="nav-links">
+          <a href="#">
+            <i class="bx bx-heart icon"></i>
+            <span class="text nav-text">Clientes</span>
+          </a>
+        </li>
+        <li class="nav-links">
+          <a href="#">
+            <i class="bx bx-wallet icon"></i>
+            <span class="text nav-text">Ganancia</span>
+          </a>
+        </li>
+      </ul>
+    </div>
+    <div class="bottom-content">
+      <li class="">
+        <a href="#">
+          <i class="bx bx-log-out icon"></i>
+          <span class="text nav-text">Log out</span>
+        </a>
+      </li>
+      <li class="mode">
+        <div class="sun-moon">
+          <i class="bx bx-moon icon moon"></i>
+          <i class="bx bx-sun icon sun"></i>
+        </div>
+        <span class="mode-text text">Modo oscuro</span>
+        <div class="toggle switch">
+          <span class="switch"></span>
+        </div>
+      </li>
+    </div>
+  </div>
+</nav>
