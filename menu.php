d4871fe9d7e303520dc406f36b75a3eabcbbdfa8
NGabrielHrz <rotsen.soher@gmail.com>
2023-06-02 13:00:05 -0600

    Terminado del codigo, falta corregir algunos detalles

--- menu.php
+++ menu.php
@@ -9,48 +9,48 @@
         <span class="profession">Tecnm</span>
       </div>
     </div>
-    <i class="bx bx-chevron-right toggle"></i>
+    <i class="bi bi-chevron-right toggle"></i>
   </header>
   <div class="menu-bar">
     <div class="menu">
       <li class="search-box">
-        <i class="bx bx-search icon"></i>
+        <i class="bi bi-search icon"></i>
         <input type="text" placeholder="Buscar...">
       </li>
       <ul class="menu-links">
         <li class="nav-links">
           <a href="#">
-            <i class="bx bx-home-alt icon"></i>
+            <i class="bi bi-house icon"></i>
             <span class="text nav-text">Panel</span>
           </a>
         </li>
         <li class="nav-links">
           <a href="#">
-            <i class="bx bx-bar-chart-alt-2 icon"></i>
+            <i class="bi bi-bar-chart icon"></i>
             <span class="text nav-text">Ganancia</span>
           </a>
         </li>
         <li class="nav-links">
           <a href="#">
-            <i class="bx bx-bell icon"></i>
+            <i class="bi bi-bell icon"></i>
             <span class="text nav-text">Notificaciones</span>
           </a>
         </li>
         <li class="nav-links">
           <a href="#">
-            <i class="bx bx-pie-chart-alt icon"></i>
+            <i class="bi bi-pie-chart icon"></i>
             <span class="text nav-text">Analizis</span>
           </a>
         </li>
         <li class="nav-links">
           <a href="#">
-            <i class="bx bx-heart icon"></i>
+            <i class="bi bi-heart icon"></i>
             <span class="text nav-text">Clientes</span>
           </a>
         </li>
         <li class="nav-links">
           <a href="#">
-            <i class="bx bx-wallet icon"></i>
+            <i class="bi bi-wallet icon"></i>
             <span class="text nav-text">Ganancia</span>
           </a>
         </li>
@@ -59,17 +59,17 @@
     <div class="bottom-content">
       <li class="">
         <a href="#">
-          <i class="bx bx-log-out icon"></i>
+          <i class="bi bi-box-arrow-left icon"></i>
           <span class="text nav-text">Log out</span>
         </a>
       </li>
       <li class="mode">
         <div class="sun-moon">
-          <i class="bx bx-moon icon moon"></i>
-          <i class="bx bx-sun icon sun"></i>
+          <i class="bi bi-moon icon moon"></i>
+          <i class="bi bi-sun icon sun"></i>
         </div>
         <span class="mode-text text">Modo oscuro</span>
-        <div class="toggle switch">
+        <div class="toggle-switch">
           <span class="switch"></span>
         </div>
       </li>
