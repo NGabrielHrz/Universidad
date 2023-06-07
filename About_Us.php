cc961513f67015f431265848394e0e1dce6f4a44
NGabrielHrz <rotsen.soher@gmail.com>
2023-06-07 14:15:59 -0600

    Terminado

---
+++ About_Us.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="description" content="">
+  <link rel="stylesheet" href="style.css">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
+  <title>University</title>
+</head>
+
+<body>
+  <?php
+  include "menu.php";
+  ?>
+  <section class="home">
+    <h1 class="text">Instituto Tecnologico de Merida</h1>
+    <div class="video-tecnm">
+      <iframe width="784" height="441" src="https://www.youtube.com/embed/IccMSGtjyTY" title="INSTITUTO TECNOLÓGICO DE MÉRIDA: ORGULLO TECNOLÓGICO" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen class="video"></iframe>
+    </div>
+  </section>
+  <script src="script.js"></script>
+</body>
+
+</html>
---
+++ We_are_located.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="description" content="">
+  <link rel="stylesheet" href="style.css">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
+  <title>University</title>
+</head>
+
+<body>
+  <?php
+  include "menu.php";
+  ?>
+  <section class="home">
+    <h1 class="text">Instituto Tecnologico de Merida (Campus poniente)</h1>
+    <div class="map-location">
+      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3596.3246707106123!2d-89.66859798778988!3d21.011302677448885!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8f5674891d01315f%3A0xcb8ba96b39a6031c!2sInstituto%20Tecnol%C3%B3gico%20de%20M%C3%A9rida%2C%20Campus%20Poniente!5e0!3m2!1ses-419!2smx!4v1686156377702!5m2!1ses-419!2smx" width="700" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade" class="map"></iframe>
+    </div>
+  </section>
+  <script src="script.js"></script>
+</body>
+
+</html>
