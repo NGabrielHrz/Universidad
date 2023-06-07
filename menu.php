e5e8abb7fcf2000a7064d15eae91e0fa634f39f7
NGabrielHrz <rotsen.soher@gmail.com>
2023-06-07 14:17:35 -0600

    Añadido para un mejor diseño visual de la pagina web

---
+++ menu.php
@@ -0,0 +1,56 @@
+<nav class="sidebar close">
+  <header>
+    <div class="image-text">
+      <span class="image">
+        <img src="img/logo_tec.png" alt="">
+      </span>
+      <div class="text logo-text">
+        <span class="name"></span>
+        <span class="profession">Tecnm</span>
+      </div>
+    </div>
+    <i class="bi bi-chevron-right toggle"></i>
+  </header>
+  <div class="menu-bar">
+    <div class="menu">
+      <ul class="menu-links">
+        <li class="nav-links">
+          <a href="index.php">
+            <i class="bi bi-house icon"></i>
+            <span class="text nav-text">Dashboard</span>
+          </a>
+        </li>
+        <li class="nav-links">
+          <a href="We_are_located.php">
+            <i class="bi bi-map icon"></i>
+            <span class="text nav-text">Ubicanos</span>
+          </a>
+        </li>
+        <li class="nav-links">
+          <a href="About_Us.php">
+            <i class="bi bi-mortarboard icon"></i>
+            <span class="text nav-text">Concocenos</span>
+          </a>
+        </li>
+      </ul>
+    </div>
+    <div class="bottom-content">
+      <li class="">
+        <a href="user-acces.php">
+          <i class="bi bi-box-arrow-in-right icon"></i>
+          <span class="text nav-text">Log in</span>
+        </a>
+      </li>
+      <li class="mode">
+        <div class="sun-moon">
+          <i class="bi bi-moon icon moon"></i>
+          <i class="bi bi-sun icon sun"></i>
+        </div>
+        <span class="mode-text text">Modo oscuro</span>
+        <div class="toggle-switch">
+          <span class="switch"></span>
+        </div>
+      </li>
+    </div>
+  </div>
+</nav>
---
+++ user-acces.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="description" content="">
+  <link rel="stylesheet" href="style.css">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
+  <title>University</title>
+</head>
+
+<body>
+  <?php
+    include "menu.php";
+  ?>
+  <div class="index">
+    <form action="login.php" method="post">
+      <div class="loging">
+        <img src="img/logo_tec.png" class="imagen">
+        <input type="email" name="user" id="user" placeholder="Usuario">
+        <input type="password" name="pass" id="pass" placeholder="Contraseña">
+        <button type="submit">
+          <i class="bi bi-box-arrow-in-right icon"></i>
+          <span class="text nav-text">Log in</span>
+        </button>
+      </div>
+    </form>
+  </div>
+  <script src="script.js"></script>
+</body>
+
+</html>
